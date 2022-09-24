8008ed28c57940d40c63cdd746ff39145ad7a27c
rohitgawade11 <110242347+rohitgawade11@users.noreply.github.com>
2022-09-24 15:23:46 +0530

    added some info in about component

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,10 @@ export default function About() {
   return (
     <>
       <div className="container">
-        {/* <div className="card py-3 text-center" style={{ backgroundColor: "transparent" }}>
+        <div className="card py-3 text-center" style={{ backgroundColor: "transparent" }}>
           <h2 className='h1'>TextUtils - word counter, character counter, remove extra spaces</h2>
-        </div> */}
-         <h2 className='h1 text-center'>TextUtils - word counter, character counter, remove extra spaces</h2>
+        </div>
+         {/* <h2 className='h1 text-center'>TextUtils - word counter, character counter, remove extra spaces</h2> */}
         <div className='my-5'>
           <h2>How TextUtils Works ?</h2>
           <p className='my-4 px-5 py-4 border-start border-5 border-dark text-light' style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>Dealing with texts is a common day-to-day activity. Here you can find all the text utilities to help you in performing your activity much faster. This includes tools to remove extra spaces, find and replace, case converter, and many others.</p>
