cf1ad16909386d9e4c08d478cbc09106dd3d6ce5
rohitgawade11 <110242347+rohitgawade11@users.noreply.github.com>
2022-09-26 09:37:33 +0530

    some isuue solved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,7 +7,7 @@ export default function TextForm(props) {
     const location = useLocation();
     const { clickEvent, example } = props;
     const context = useContext(TextContext);
-    const { text, handleOnChange, find, replace, setFind, setReplace,handleClearClick,handleCopy } = context;
+    const { text, handleOnChange, find, replace, setFind, setReplace, handleClearClick, handleCopy } = context;
 
     const handleOnChangeFind = (event) => {
         setFind(event.target.value);
@@ -32,13 +32,11 @@ export default function TextForm(props) {
                                         <input type="text" value={find} name="find" placeholder='find' onChange={handleOnChangeFind} id="find" className="form-control mx-1" />
                                         <input type="text" value={replace} name="replace" placeholder='replace' onChange={handleOnChangeReplace} id="replace" className="form-control mx-1" />
                                     </div>}
-                                    <div className='self-center'>
-                                    <button className="btn btn-success mx-1" disabled={text.length===0} onClick={clickEvent}>Submit</button>
-                                <button className="btn btn-danger" disabled={text.length===0} onClick={handleClearClick}>Clear Text</button>
-                                    </div>
-                              
+                                <div className='self-center'>
+                                    <button className="btn btn-success mx-1" disabled={location.pathname === '/find-and-replace' ? find.length === 0 || text.length === 0 : text.length === 0} onClick={clickEvent}>Submit</button>
+                                    <button className="btn btn-danger" disabled={text.length === 0} onClick={handleClearClick}>Clear Text</button>
+                                </div>
                             </div>
-
                         </div>
                     </div>
                     <div className="col-xl-6">
@@ -49,7 +47,7 @@ export default function TextForm(props) {
 
                                 <p className=' mx-2'>Words : {text.split(/\s+/).filter((element) => { return element.length !== 0 }).length}</p>
                                 <p className=' mx-2'>Characters : {text.split(/\w/).length - 1}</p>
-                                <button className="btn btn-success mx-2" disabled={text.length===0} onClick={handleCopy}>Copy to clipboard</button>
+                                <button className="btn btn-success mx-2" disabled={text.length === 0} onClick={handleCopy}>Copy to clipboard</button>
                             </div>
                         </div>
                     </div>
@@ -72,25 +70,6 @@ export default function TextForm(props) {
                     </div>
                 </div>
             </div>
-            {/* <div className="container" style={{color:'black'}}> 
-            <h1 className='mb-4'>{props.heading}</h1>
-            <div className="mb-3"> 
-            <label htmlFor="myBox" className='form-label' style={{fontWeight:"500",fontSize:"1.4rem"}}>Enter your text here :</label>
-            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
-            </div>
-            <button disabled={text.length===0}  className="btn btn-secondary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-            <button disabled={text.length===0}  className="btn btn-secondary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-            <button disabled={text.length===0}  className="btn btn-secondary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-            <button disabled={text.length===0}  className="btn btn-secondary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-            <button disabled={text.length===0}  className="btn btn-secondary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-        </div>
-        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
-            <h2>Summary of Your Text</h2>
-            <p>Number of Words : {text.split(/\s+/).filter((element)=>{return element.length!==0}).length}</p>
-            <p>Number of Characters : {text.split(/\w/).length - 1} characters</p>
-            <h2 style={{textAlign:"center"}}>Preview</h2>
-            <textarea className="form-control" readOnly value={text.length > 0 ? text : "Nothing to preview"} style={{backgroundColor: props.mode==='dark'?'black':'white', color: props.mode==='dark'?'white':'#042743'}} id="previw" rows="4"></textarea>
-        </div> */}
         </>
     )
 }
