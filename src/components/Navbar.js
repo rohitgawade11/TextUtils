184ab21f9d95b4e1c87eebd0058080654b420f69
rohitgawade11 <110242347+rohitgawade11@users.noreply.github.com>
2023-02-13 20:12:25 +0530

    minor change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark custom-nav">
-            <div className="container-fluid">
+            <div className="container">
                 <Link className="navbar-brand" to="/">{props.title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
